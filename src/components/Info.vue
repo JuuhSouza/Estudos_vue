f9d549d5f37634198f6c7bee7aa315cb9ad091fb
JuuhSouza <juliasouzak4@gmail.com>
2026-01-03 20:29:54 -0300

    estudando vue

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,16 +1,25 @@
 <template>
 <div>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus minus voluptas non fuga tempora, expedita iusto culpa atque aperiam ratione maxime ipsum, dolore, officiis quibusdam veritatis sed et reiciendis. Optio!</p>
+    <p v-if="esta_trabalhando">Esrtou tentando trabalhar aq!</p> <!-- sempre trabalhar com if e else um ao lado do outro, pois se colocar em linhas oportas ela n irá reconhecer o if -->
+    <p v-else> Em busca de um milhagre</p>
         <p>Tecnologias aprendidas: {{  }}</p>
         <ul>
             <li> JavaScript</li>
             <li> Vue.js </li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para : {{ email }}</p>
 </div>
 </template>
  
 <script>
     export default{
-        name : 'Info'
+        name : 'Info',
+        data(){
+            return{
+                esta_trabalhando : true,
+                mostrar_email: true,
+                email: 'Julia@gmail.com'
+            }
+        }
     }
  </script>
--- src/components/Pessoa.vue
+++ src/components/Pessoa.vue
@@ -2,16 +2,18 @@
     <div>
         <h2>Essa é sua descrição: {{ nome }} </h2>
         <Info/>
+        <Form/>
     </div>
 </template>
 
 <script>
     import Info from './Info.vue'
+import Form from './Form.vue'
     
     export default{
         name : 'Pessoa',
         components:{
-            Info
+            Info, Form
         },
         data(){
             return{
